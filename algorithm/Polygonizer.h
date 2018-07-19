60f105a0d2a46e44e264c5d20c2976fce8a6701e
dongmin.kim <dongmin.kim@pusan.ac.kr>
2018-07-19 17:14:16 +0900

    polygonizer in algorithm namespace

---
+++ algorithm/Polygonizer.h
@@ -0,0 +1,43 @@
+//
+// Created by dongmin on 18. 7. 19.
+//
+
+#ifndef TM2IN_POLYGONIZER_H
+#define TM2IN_POLYGONIZER_H
+
+#include "features/PolyhedralSurface.h"
+
+#include <SFCGAL/Polygon.h>
+#include <vector>
+
+using namespace std;
+
+namespace TM2IN {
+    namespace algorithm {
+        class Polygonizer {
+        public:
+            virtual void run(PolyhedralSurface* space) = 0;
+        };
+
+        class TrianglePolygonizer : public Polygonizer{
+        public:
+            void run(PolyhedralSurface* space);
+        };
+
+        class DividedPolygonizer : public Polygonizer {
+        public:
+            void run(PolyhedralSurface* space);
+        };
+
+        class PCAPolygonizer : public Polygonizer {
+        public:
+            void run(PolyhedralSurface* space);
+        private:
+            void scale_up(vector<Vertex *> &vertices, double scale);
+            SFCGAL::Polygon make_sf_polygon(vector<Vertex *> vertices);
+        };
+    }
+}
+
+
+#endif //TM2IN_POLYGONIZER_H
