57dcd7ea27e8ee99d0322ef66c4dc57ef820cd27
dongmin.kim <dongmin.kim@pusan.ac.kr>
2018-08-07 16:15:08 +0900

    mainpage and wall dir

---
+++ doc/mainpage.h
@@ -0,0 +1,8 @@
+//
+// Created by dongmin on 18. 8. 7.
+//
+
+#ifndef TM2IN_MAINPAGE_H
+#define TM2IN_MAINPAGE_H
+
+#endif //TM2IN_MAINPAGE_H
---
+++ features/Wall/Surface.h
@@ -0,0 +1,8 @@
+//
+// Created by dongmin on 18. 8. 7.
+//
+
+#ifndef TM2IN_SURFACE_H
+#define TM2IN_SURFACE_H
+
+#endif //TM2IN_SURFACE_H
