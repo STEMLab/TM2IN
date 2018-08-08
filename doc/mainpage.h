9b6fc432bac7a21cb2548bde25b20db898e3de9d
dongmin.kim <dongmin.kim@pusan.ac.kr>
2018-08-08 10:19:02 +0900

    doc

--- doc/mainpage.h
+++ doc/mainpage.h
@@ -1,8 +1,31 @@
-//
-// Created by dongmin on 18. 8. 7.
-//
-
-#ifndef TM2IN_MAINPAGE_H
-#define TM2IN_MAINPAGE_H
-
-#endif //TM2IN_MAINPAGE_H
+/**
+ *
+ * @mainpage TM2IN
+ *
+ * TM2IN generates appropriate Solid geometry for Indoor Map from Triangle Mesh data such as TVR, COLLADA, 3DS.
+ *
+ * @section intro_sec Introduction
+ *
+ * @section dependencies_sec Dependencies
+ *
+- CGAL (http://www.cgal.org/)
+- SFCGAL (http://www.sfcgal.org)
+- Eigen3 (http://eigen.tuxfamily.org/index.php?title=Main_Page)
+- Boost (http://www.boost.org/)
+- rapidxml (http://rapidxml.sourceforge.net)
+ *
+ * @section geometric_model_sec Geometric model
+ *
+ * @section api API
+ *
+ * Just for users who wants to use converter, see @link Converter Converter. @endlink
+ *
+ * Functions for light users are posted in @link public_api Public API @endlink
+ *
+ * Implementation details are posted in @link protected_api Protected API @endlink
+ *
+ * @defgroup public_api Public API
+ *
+ * @defgroup protected_api Protected API
+ *
+ */
